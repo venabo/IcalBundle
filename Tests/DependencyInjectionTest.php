d3235907b735d0a85cc6b68e6507e0162956f760
nkiewitt <n.kiewitt@venabo.de>
2024-07-17 12:03:44 +0200

    - removed: dependence of symfony/framework-bundle and reduced it, to the really needed packages - added: return type to BOMOIcalExtension.php function load

--- Tests/DependencyInjectionTest.php
+++ Tests/DependencyInjectionTest.php
@@ -2,26 +2,19 @@
 
 namespace BOMO\IcalBundle\Tests;
 
-
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class DependencyInjectionTest extends TestCase
 {
-    /**
-     * @return TestKernel
-     */
-    protected function buildKernel()
-    {
-        $kernel = new TestKernel(__DIR__.'/../Resources/config/services.xml');
-        $kernel->boot();
-
-        return $kernel;
-    }
-
     public function testServicesRegistration()
     {
-        $kernel = $this->buildKernel();
-        $container = $kernel->getContainer();
+        $container = new ContainerBuilder();
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+        $container->compile();
 
         $key = 'bomo_ical.ics_provider';
         $this->assertTrue($container->has($key), "Service $key doesn't seem to be registered");
